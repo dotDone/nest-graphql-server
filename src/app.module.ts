7b5c62094a39664d9ef52346f22ceb3e43aada96
dotDone <liam.simpson.91@gmail.com>
2022-02-02 21:53:51 +0800

    Hiden MongoDB creds & updated linter settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
+import 'dotenv/config'
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
-
 import { UsersModule } from './modules/users/users.module'
 
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot('mongodb+srv://graphql:HK9cc0eTxKSfav2E@main.4rcui.mongodb.net/main?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_ADDRESS}?retryWrites=true&w=majority`),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     })],
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,11 @@
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from "@nestjs/mongoose"
 import { Module } from "@nestjs/common"
 import { UsersResolver } from "./users.resolver"
-import { UserSchema } from 'src/modules/users/user.schema'
-import { UsersService } from './users.service'
+import { UserSchema } from "src/modules/users/user.schema"
+import { UsersService } from "./users.service"
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [UsersResolver, UsersService]
+  providers: [UsersResolver, UsersService],
 })
 export class UsersModule { }
